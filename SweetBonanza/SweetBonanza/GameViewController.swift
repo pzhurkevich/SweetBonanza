a4b455376d966e3a8b112f9f322cfcb3baabdf1c
Pavel Zhurkevich <>
2024-02-08 11:01:23 +0300

    ADD Images + Loading VC

--- SweetBonanza/SweetBonanza/GameViewController.swift
+++ SweetBonanza/SweetBonanza/GameViewController.swift
@@ -11,25 +11,37 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
+    // MARK: - SKScene
+
+    private lazy var skView: SKView = {
+        let view = SKView()
+        view.ignoresSiblingOrder = true
+        view.showsFPS = true
+        view.showsNodeCount = true
+        let scene = SKScene(fileNamed: "GameScene")
+        if let scene = SKScene(fileNamed: "GameScene") {
+            scene.scaleMode = .aspectFill
+            scene.size = self.view.frame.size
+            view.presentScene(scene)
+        }
+        return view
+    }()
+
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        navigationController?.navigationBar.isHidden = true
+        view.addSubview(skView)
+        makeConstraints()
     }
 
+    // MARK: - Constraints
+
+    private func makeConstraints() {
+        skView.snp.makeConstraints { make in
+            make.size.equalToSuperview()
+        }
+    }
 
 }
--- SweetBonanza/SweetBonanza/LoadingViewController.swift
+++ SweetBonanza/SweetBonanza/LoadingViewController.swift
@@ -10,10 +10,84 @@ import SnapKit
 
 class LoadingViewController: UIViewController {
 
+    // MARK: - GUI variables
+
+    private lazy var backgroundImage: UIImageView = {
+        let view = UIImageView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.image = UIImage(named: "loadingBG")
+        view.contentMode = .scaleAspectFill
+        return view
+    }()
+
+    private lazy var logoImage: UIImageView = {
+        let view = UIImageView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.image = UIImage(named: "logo")
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+
+    private lazy var loadingTitle: UIImageView = {
+        let view = UIImageView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.image = UIImage(named: "loadingTitle")
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.addSubview(backgroundImage)
+        view.addSubview(logoImage)
+        view.addSubview(loadingTitle)
+        view.sendSubviewToBack(backgroundImage)
+        setContentConstraints()
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        animate()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+            let vc = GameViewController()
+            vc.modalPresentationStyle = .formSheet
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 
+    // MARK: - Constraints
+
+    private func setContentConstraints() {
+        backgroundImage.snp.makeConstraints { make in
+            make.top.left.bottom.right.equalToSuperview()
+        }
+        logoImage.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(800)
+            make.size.equalTo(100)
+        }
+        loadingTitle.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(70)
+            make.height.equalTo(30)
+            make.width.equalTo(100)
+        }
+    }
+
+    // MARK: - Actions
+
+    private func animate() {
+
+        UIView.animate(withDuration: 3) {
+            self.logoImage.snp.updateConstraints { make in
+                make.centerY.equalToSuperview()
+            }
+            self.view.layoutIfNeeded()
+        }
+        UIView.animate(withDuration: 1, delay: 0, options: [.repeat, .autoreverse], animations: {
+            self.loadingTitle.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+        })
+    }
 }
