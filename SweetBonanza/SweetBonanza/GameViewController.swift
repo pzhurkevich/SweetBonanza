0d543ae18086a83e6605680d59450e97740f3727
Pavel Zhurkevich <>
2024-02-08 10:55:17 +0300

    ADDED SnapKit, DELETE Storyboard

--- SweetBonanza/SweetBonanza/GameViewController.swift
+++ SweetBonanza/SweetBonanza/GameViewController.swift
@@ -31,15 +31,5 @@ class GameViewController: UIViewController {
         }
     }
 
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
 
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
 }
---
+++ SweetBonanza/SweetBonanza/LoadingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoadingViewController.swift
+//  SweetBonanza
+//
+//  Created by Pavel on 8.02.24.
+//
+
+import UIKit
+import SnapKit
+
+class LoadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
