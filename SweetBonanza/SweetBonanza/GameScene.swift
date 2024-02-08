76858c2d473cd0422934c46b8de92960897a9be3
Pavel Zhurkevich <>
2024-02-08 15:52:19 +0300

    FIX code corrections

--- SweetBonanza/SweetBonanza/GameScene.swift
+++ SweetBonanza/SweetBonanza/GameScene.swift
@@ -9,23 +9,23 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-
+    
     // MARK: - Variables
-
+    
     let cardsName = ["01", "02", "03", "04", "05", "06", "07", "08"]
-
+    
     var score: Int = 0
     var cardCount: Int = 0
-
+    
     var nameCount: [String: Int] = [:]
     var removerPositions: [CGPoint] = []
-
+    
     lazy var startPosition: CGFloat = -container.size.width/2 + 30
-
+    
     // MARK: - Sprite variables
-
+    
     var background = SKSpriteNode(imageNamed: "mainBG")
-
+    
     lazy var scoreLabel: SKLabelNode = {
         let view = SKLabelNode()
         view.position = CGPoint(x: 0.5, y: self.size.width/1.5)
@@ -35,14 +35,14 @@ class GameScene: SKScene {
         view.text = "Score \(score)"
         return view
     }()
-
+    
     lazy var container: SKSpriteNode = {
         let view = SKSpriteNode(imageNamed: "cardContainer")
         view.position = CGPoint(x: 0, y: -self.size.width/2)
         view.size = CGSize(width: self.size.width - 20 , height: self.size.width/7)
         return view
     }()
-
+    
     lazy var pauseButton: SKSpriteNode = {
         let view = SKSpriteNode(imageNamed: "pause")
         view.name = "pause"
@@ -50,7 +50,7 @@ class GameScene: SKScene {
         view.size = CGSize(width: self.size.width/10 , height: self.size.width/10)
         return view
     }()
-
+    
     lazy var dimSprite: SKSpriteNode = {
         let view = SKSpriteNode()
         view.color = UIColor.black.withAlphaComponent(0.5)
@@ -60,7 +60,7 @@ class GameScene: SKScene {
         view.name = "dimSprite"
         return view
     }()
-
+    
     lazy var bubblesBackground: SKSpriteNode = {
         let bubblesBackground = SKSpriteNode(imageNamed: "bubbles")
         bubblesBackground.position = CGPoint(x: 0, y: 0)
@@ -69,9 +69,9 @@ class GameScene: SKScene {
         bubblesBackground.size = .init(width: self.size.width * 2, height: self.size.height)
         return bubblesBackground
     }()
-
+    
     // MARK: - Scene didMove
-
+    
     override func didMove(to view: SKView) {
         addChild(container)
         addChild(scoreLabel)
@@ -80,15 +80,15 @@ class GameScene: SKScene {
         createCardStack()
         containerFillWithHolders()
     }
-
+    
     // MARK: - Sprite setters
-
+    
     private func setBackground() {
-            background.zPosition = -2
-            background.size = CGSize(width: self.size.width, height: self.size.height)
-            addChild(self.background)
+        background.zPosition = -2
+        background.size = CGSize(width: self.size.width, height: self.size.height)
+        addChild(self.background)
     }
-
+    
     private func createCardStack() {
         for i in stride(from: -self.size.width/3 + 40, to: self.size.width/3 - 40, by: self.size.width/7) {
             for j in stride(from: 0, to: self.size.width/2, by: self.size.width/7) {
@@ -101,9 +101,9 @@ class GameScene: SKScene {
             }
         }
     }
-
+    
     private func containerFillWithHolders() {
-
+        
         for i in 0...6 {
             let holder = SKSpriteNode(texture: SKTexture(imageNamed: "cardHolder"))
             holder.size = .init(width: self.size.width/10, height: self.size.width/10)
@@ -114,64 +114,32 @@ class GameScene: SKScene {
             background.addChild(holder)
         }
     }
-
-    private func looseAction() {
-
-        container.removeAllChildren()
-        score = 0
-        cardCount = 0
-
-        nameCount = [:]
-        removerPositions = []
-        startPosition = -container.size.width/2 + 30
-
-
-        addChild(dimSprite)
-        dimSprite.addChild(bubblesBackground)
-
-        let frame = SKSpriteNode(imageNamed: "settings")
-        frame.position = CGPoint(x: 0, y: 0)
-        frame.zPosition = 1
-        frame.size = .init(width: 200, height: 150)
-        dimSprite.addChild(frame)
-
-
-        let title = SKLabelNode()
-        title.position = CGPoint(x: 0, y: 0)
-        title.fontColor = .black
-        title.fontSize = 30
-        title.zPosition = 2
-        title.fontName = "Helvetica-Bold"
-        title.text = "YOU LOSE"
-
-        frame.addChild(title)
-
-    }
-
+    
+    
     // MARK: - Touch Actions
-
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if container.children.isEmpty {
             removerPositions = []
             startPosition = -container.size.width/2 + 30
         }
-
+        
         guard let touch = touches.first else { return }
         let location = touch.location(in: self)
-
-
+        
+        
         guard let sprite = nodes(at: location).first as? SKSpriteNode else { return }
-
+        
         switch sprite.name {
         case pauseButton.name:
             openSettings()
             
         case background.name:
             break
-
+            
         case dimSprite.name, bubblesBackground.name:
             dismissSettings()
-
+            
         default :
             guard location.y > -50 else { break }
             tappingOnCardAction(for: sprite)
@@ -180,9 +148,9 @@ class GameScene: SKScene {
 }
 
 extension GameScene {
-
+    
     // MARK: - Actions
-
+    
     private func tappingOnCardAction(for sprite: SKSpriteNode) {
         guard cardCount < 7 else { return }
         cardCount += 1
@@ -201,16 +169,16 @@ extension GameScene {
         findAndAddDuplicateNames(from: container.children)
         // FIXME: - add action
         if cardCount == 7 {
-           looseAction()
+            looseAction()
         }
     }
-
+    
     private func dismissSettings() {
         dimSprite.removeAllChildren()
         bubblesBackground.removeFromParent()
         dimSprite.removeFromParent()
     }
-
+    
     private func addCardToRemovedPosition(removed: SKSpriteNode) {
         guard let name = cardsName.randomElement() else { return }
         let cardTexture = SKSpriteNode(texture: SKTexture(imageNamed: name))
@@ -219,34 +187,34 @@ extension GameScene {
         cardTexture.name = name
         addChild(cardTexture)
     }
-
+    
     private func openSettings() {
         addChild(dimSprite)
         dimSprite.addChild(bubblesBackground)
-
+        
         let frame = SKSpriteNode(imageNamed: "settings")
         frame.position = CGPoint(x: 0, y: 0)
         frame.zPosition = 1
         frame.size = .init(width: 200, height: 150)
         dimSprite.addChild(frame)
-
+        
         let settingsTitle = SKSpriteNode(imageNamed: "settingsTitle")
         settingsTitle.position = CGPoint(x: 0, y: frame.size.height/2 + 40)
         settingsTitle.zPosition = 1
         settingsTitle.size = .init(width: 200, height: 50)
         dimSprite.addChild(settingsTitle)
     }
-
-    private func findAndAddDuplicateNames(from array: [SKNode]){
+    
+    private func findAndAddDuplicateNames(from array: [SKNode]) {
         var nameCount: [String: Int] = [:]
         var duplicateNodes: [SKNode] = []
-
+        
         for node in array {
             if let name = node.name {
                 nameCount[name, default: 0] += 1
             }
         }
-
+        
         for node in array {
             if let name = node.name, nameCount[name]! >= 3 {
                 duplicateNodes.append(node)
@@ -265,15 +233,41 @@ extension GameScene {
             scoreLabel.text = "Score \(score)"
         }
     }
-}
-
-extension SKNode {
-    func removeNodeAtPosition(_ position: CGPoint) {
-        for node in children {
-            if node.position == position {
-                node.removeFromParent()
-                break
-            }
-        }
+    
+    private func clearStartParametrs() {
+        container.removeAllChildren()
+        score = 0
+        cardCount = 0
+        
+        nameCount = [:]
+        removerPositions = []
+        startPosition = -container.size.width/2 + 30
+        scoreLabel.text = "Score \(0)"
+    }
+    
+    private func looseAction() {
+        
+        clearStartParametrs()
+        addChild(dimSprite)
+        dimSprite.addChild(bubblesBackground)
+        
+        let frame = SKSpriteNode(imageNamed: "settings")
+        frame.position = CGPoint(x: 0, y: 0)
+        frame.zPosition = 1
+        frame.size = .init(width: 200, height: 150)
+        dimSprite.addChild(frame)
+        
+        
+        let title = SKLabelNode()
+        title.position = CGPoint(x: 0, y: 0)
+        title.fontColor = .black
+        title.fontSize = 30
+        title.zPosition = 2
+        title.fontName = "Helvetica-Bold"
+        title.text = "YOU LOSE"
+        
+        frame.addChild(title)
+        
     }
 }
+
